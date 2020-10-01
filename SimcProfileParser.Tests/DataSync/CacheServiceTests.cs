5733b1a13828b1d632327d51b2209305021dedcd
Michael Wenham <niphyr@gmail.com>
2020-10-02 01:21:24 +1000

    Implemented basic download/caching of external files.

---
+++ SimcProfileParser.Tests/DataSync/CacheServiceTests.cs
@@ -0,0 +1,136 @@
+﻿using NUnit.Framework;
+using SimcProfileParser.DataSync;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SimcProfileParser.Tests.DataSync
+{
+    /// <summary>
+    /// TODO: Create better tests for the CacheService by abstracting the File/Web access components.
+    /// Then test that this service does what it should, and seperately test the file/web work 
+    /// just once instead of for each overall test - unit-testify it rather than integration test.
+    /// </summary>
+    [TestFixture]
+    public class CacheServiceTests
+    {
+        [SetUp]
+        public void Init()
+        {
+            ICacheService cache = new CacheService();
+
+            // Wipe out the directory before testing as a workaround for file access not being abstracted
+            if(Directory.Exists(cache.BaseFileDirectory))
+                Directory.Delete(cache.BaseFileDirectory, true);
+        }
+
+        [Test]
+        /// Integration test of sorts. Checking the file download works.
+        public void CS_Downloads_File()
+        {
+            // Arrange
+            ICacheService cache = new CacheService();
+
+            cache.RegisterFileConfiguration(SimcFileType.ItemDataInc,
+                "ItemData.raw",
+                "https://raw.githubusercontent.com/simulationcraft/simc/shadowlands/engine/dbc/generated/item_data.inc"
+                );
+
+            // Act
+            var data = cache.GetFileContents(SimcFileType.ItemDataInc);
+
+            // Assert
+            DirectoryAssert.Exists(cache.BaseFileDirectory);
+            FileAssert.Exists(Path.Combine(cache.BaseFileDirectory, "ItemData.raw"));
+            Assert.NotNull(data);
+        }
+
+        [Test]
+        public void CS_Cache_Updates()
+        {
+            // Arrange
+            CacheService cache = new CacheService();
+
+            // Act
+            var filename = "test.txt";
+            var eTag = "12345";
+            var fileContents = @"[
+  {
+    ""Filename"": ""test.txt"",
+    ""ETag"": ""12345""
+  }
+]";
+
+            cache.UpdateCacheData(filename, eTag);
+            var data = File.ReadAllText(Path.Combine(cache.BaseFileDirectory, "FileDownloadCache.json"));
+
+            // Assert
+            FileAssert.Exists(Path.Combine(cache.BaseFileDirectory, "FileDownloadCache.json"));
+            Assert.AreEqual(fileContents, data);
+        }
+
+        [Test]
+        public void CS_Cache_Reads()
+        {
+            // Arrange
+            CacheService cache = new CacheService();
+
+            // Act
+            var filename = "test.txt";
+            var eTag = "12345";
+            cache.UpdateCacheData(filename, eTag);
+            var cacheData = cache.GetCacheData();
+
+            // Assert
+            Assert.IsNotNull(cacheData);
+            Assert.NotZero(cacheData.Count);
+            Assert.AreEqual(filename, cacheData.FirstOrDefault().Filename);
+            Assert.AreEqual(eTag, cacheData.FirstOrDefault().ETag);
+        }
+
+        [Test]
+        public void CS_Cache_Saves()
+        {
+            // Arrange
+            CacheService cache = new CacheService();
+
+            // Act
+            var filename = "test.txt";
+            var eTag = "12345";
+            var eTagEntry = new FileETag()
+            {
+                Filename = filename,
+                ETag = eTag
+            };
+            var entries = new List<FileETag>()
+            {
+                eTagEntry
+            };
+            var fileContents = @"[
+  {
+    ""Filename"": ""test.txt"",
+    ""ETag"": ""12345""
+  }
+]";
+
+            cache.SaveCacheData(entries);
+            var data = File.ReadAllText(Path.Combine(cache.BaseFileDirectory, "FileDownloadCache.json"));
+
+
+            // Assert
+            Assert.IsNotNull(data);
+            FileAssert.Exists(Path.Combine(cache.BaseFileDirectory, "FileDownloadCache.json"));
+            Assert.AreEqual(fileContents, data);
+        }
+
+        [OneTimeTearDown]
+        public void TearDownOnce()
+        {
+            ICacheService cache = new CacheService();
+            if (Directory.Exists(cache.BaseFileDirectory))
+                Directory.Delete(cache.BaseFileDirectory, true);
+        }
+    }
+}
---
+++ SimcProfileParser/DataSync/CacheService.cs
@@ -0,0 +1,235 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace SimcProfileParser.DataSync
+{
+    internal interface ICacheService
+    {
+        string BaseFileDirectory { get; }
+        IReadOnlyCollection<CacheFileConfiguration> RegisteredFiles { get; }
+        string GetFileContents(SimcFileType FileType);
+        bool DownloadFileIfChanged(Uri sourceUri, Uri destinationUri, string eTag);
+        bool DownloadFile(Uri sourceUri, Uri destinationUri);
+        void RegisterFileConfiguration(SimcFileType fileType, string localFilename, string remoteFilename);
+    }
+
+    internal class CacheService : ICacheService
+    {
+        public string BaseFileDirectory { get; }
+        protected IList<CacheFileConfiguration> _registeredFiles;
+
+        public IReadOnlyCollection<CacheFileConfiguration> RegisteredFiles => new ReadOnlyCollection<CacheFileConfiguration>(_registeredFiles);
+
+        public CacheService()
+        {
+            _registeredFiles = new List<CacheFileConfiguration>();
+
+            BaseFileDirectory = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory, "SimcProfileParserData");
+
+            ETagCacheData = GetCacheData();
+        }
+
+        public CacheService(IList<CacheFileConfiguration> registeredFiles)
+            : this()
+        {
+            _registeredFiles = registeredFiles;
+        }
+
+        string ICacheService.GetFileContents(SimcFileType FileType)
+        {
+            var fileConfig = _registeredFiles.Where(f => f.FileType == FileType).FirstOrDefault();
+
+            if (fileConfig != null)
+            {
+                var eTag = ETagCacheData
+                    .Where(e => e.Filename == fileConfig.LocalFile.LocalPath)
+                    .FirstOrDefault();
+
+                // If there is no cache data, or the file doesn't exist then re-fetch it
+                if (eTag == null || !File.Exists(fileConfig.LocalFile.LocalPath))
+                    eTag = null;
+
+                ((ICacheService)this).DownloadFileIfChanged(
+                    fileConfig.RemoteFile, fileConfig.LocalFile, eTag?.ETag);
+
+                var data = File.ReadAllText(fileConfig.LocalFile.LocalPath);
+
+                return data;
+            }
+            else
+                return "";
+        }
+
+        bool ICacheService.DownloadFile(Uri sourceUri, Uri destinationUri)
+        {
+            WebClient client = new WebClient();
+
+            try
+            {
+                var baseDirectory = new Uri(destinationUri, ".");
+                if (!Directory.Exists(baseDirectory.OriginalString))
+                    Directory.CreateDirectory(baseDirectory.OriginalString);
+
+                client.DownloadFile(sourceUri, destinationUri.LocalPath);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        bool ICacheService.DownloadFileIfChanged(Uri sourceUri, Uri destinationUri, string eTag)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            HttpRequestMessage request =
+               new HttpRequestMessage(HttpMethod.Head,
+                  sourceUri);
+
+            HttpResponseMessage response;
+
+            try
+            {
+                response = httpClient.SendAsync(request).Result;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            if (eTag != null && response.Headers.ETag.Tag == eTag)
+                return true;
+
+            var downloadResponse = ((ICacheService)this).DownloadFile(sourceUri, destinationUri);
+
+            // If the download was successful, save the etag.
+            if(downloadResponse)
+            {
+                UpdateCacheData(destinationUri.LocalPath, response.Headers.ETag.Tag);
+            }
+
+            return downloadResponse;
+        }
+
+        void ICacheService.RegisterFileConfiguration(SimcFileType fileType, string localFilename, string remoteFilename)
+        {
+            var exists = _registeredFiles.Where(f => f.FileType == fileType).FirstOrDefault();
+
+            if(exists != null)
+            {
+                _registeredFiles.Remove(exists);
+            }
+
+            var configuration = new CacheFileConfiguration()
+            {
+                FileType = fileType,
+                RemoteFile = new Uri(remoteFilename),
+                LocalFile = new Uri(Path.Combine(BaseFileDirectory, localFilename))
+            };
+
+            _registeredFiles.Add(configuration);
+        }
+
+        #region eTag Cache
+
+        private readonly string _etagCacheDataFile = "FileDownloadCache.json";
+        private List<FileETag> ETagCacheData = new List<FileETag>();
+
+        /// <summary>
+        /// Update the cache with an entry
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <param name="eTag"></param>
+        internal void UpdateCacheData(string filename, string eTag)
+        {
+            if (ETagCacheData.Count == 0)
+                ETagCacheData = GetCacheData();
+
+            var existing = ETagCacheData.Where(e => e.Filename == filename).FirstOrDefault();
+
+            if (existing != null)
+                existing.ETag = eTag;
+            else
+            {
+                ETagCacheData.Add(new FileETag()
+                {
+                    Filename = filename,
+                    ETag = eTag
+                });
+            }
+
+            SaveCacheData(ETagCacheData);
+        }
+
+        /// <summary>
+        /// Load the cached etag data from file
+        /// </summary>
+        internal List<FileETag> GetCacheData()
+        {
+            var results = new List<FileETag>();
+            var cacheDataFile = Path.Combine(BaseFileDirectory, _etagCacheDataFile);
+
+            if (File.Exists(cacheDataFile))
+            {
+                var data = File.ReadAllText(cacheDataFile);
+
+                var deserialised = JsonConvert.DeserializeObject<List<FileETag>>(data);
+
+                if (deserialised != null)
+                    results = deserialised;
+            }
+
+            return results;
+        }
+
+        /// <summary>
+        /// Save the cached etag data to file
+        /// </summary>
+        internal void SaveCacheData(List<FileETag> data)
+        {
+            var baseDirectory = new Uri(BaseFileDirectory);
+            if (!Directory.Exists(baseDirectory.OriginalString))
+                Directory.CreateDirectory(baseDirectory.OriginalString);
+
+            var cacheDataFile = Path.Combine(BaseFileDirectory, _etagCacheDataFile);
+            var dataString = JsonConvert.SerializeObject(data, Formatting.Indented);
+
+            File.WriteAllText(cacheDataFile, dataString);
+        }
+
+        #endregion
+    }
+
+    public class CacheFileConfiguration
+    {
+        internal SimcFileType FileType { get; set; }
+        internal Uri LocalFile { get; set; }
+        internal Uri RemoteFile { get; set; }
+    }
+
+    public class FileETag
+    {
+        public string Filename { get; set; }
+        public string ETag { get; set; }
+    }
+
+    public enum SimcFileType
+    {
+        ScSpellDataInc,
+        ItemDataInc,
+        RandomPropPointsInc,
+        ScaleDataInc,
+        ScEnumsHpp,
+        DataEnumsHh
+    }
+}
