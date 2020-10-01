66d72fac7b295723b7cf8a611af041f088745ca0
Michael Wenham <niphyr@gmail.com>
2020-10-02 02:19:26 +1000

    Added generation of CR and Stam multipliers

---
+++ SimcProfileParser.Tests/DataSync/RawDataExtractionServiceTests.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using SimcProfileParser.DataSync;
+using SimcProfileParser.Interfaces.DataSync;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SimcProfileParser.Tests.DataSync
+{
+    [TestFixture]
+    public class RawDataExtractionServiceTests
+    {
+        [Test]
+        public void RDE_Generates_CR_Multi()
+        {
+            // Arrange
+            ICacheService cacheService = new CacheService();
+            IRawDataExtractionService rawDataExtractionService = 
+                new RawDataExtractionService(cacheService);
+
+            var filepath = Path.Combine(cacheService.BaseFileDirectory, "CombatRatingMultipliers.json");
+
+            // Act
+            rawDataExtractionService.GenerateCombatRatingMultipliers();
+            var rawData = File.ReadAllText(filepath);
+            var data = JsonConvert.DeserializeObject<float[][]>(rawData);
+
+            // Assert
+            FileAssert.Exists(filepath);
+            Assert.IsNotNull(data);
+            Assert.AreEqual(4, data.Length);
+            Assert.AreEqual(1300, data[0].Length);
+        }
+
+        [Test]
+        public void RDE_Generates_Stam_Multi()
+        {
+            // Arrange
+            ICacheService cacheService = new CacheService();
+            IRawDataExtractionService rawDataExtractionService =
+                new RawDataExtractionService(cacheService);
+
+            var filepath = Path.Combine(cacheService.BaseFileDirectory, "StaminaMultipliers.json");
+
+            // Act
+            rawDataExtractionService.GenerateStaminaMultipliers();
+            var rawData = File.ReadAllText(filepath);
+            var data = JsonConvert.DeserializeObject<float[][]>(rawData);
+
+            // Assert
+            FileAssert.Exists(filepath);
+            Assert.IsNotNull(data);
+            Assert.AreEqual(4, data.Length);
+            Assert.AreEqual(1300, data[0].Length);
+        }
+    }
+}
---
+++ SimcProfileParser/DataSync/RawDataExtractionService.cs
@@ -0,0 +1,95 @@
+﻿using Newtonsoft.Json;
+using SimcProfileParser.Interfaces.DataSync;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SimcProfileParser.DataSync
+{
+    internal interface IRawDataExtractionService
+    {
+        void GenerateCombatRatingMultipliers();
+        void GenerateStaminaMultipliers();
+    }
+
+    /// <summary>
+    /// The purpose of thie class is to process a raw file into 
+    /// </summary>
+    internal class RawDataExtractionService : IRawDataExtractionService
+    {
+        private readonly ICacheService _cacheService;
+
+        public RawDataExtractionService(ICacheService cacheService)
+        {
+            _cacheService = cacheService;
+
+            _cacheService.RegisterFileConfiguration(
+                Model.DataSync.SimcFileType.ScaleDataInc,
+                "ScaleData.raw",
+                "https://raw.githubusercontent.com/simulationcraft/simc/shadowlands/engine/dbc/generated/sc_scale_data.inc");
+        }
+
+        void IRawDataExtractionService.GenerateCombatRatingMultipliers()
+        {
+            var rawData = _cacheService.GetFileContents(Model.DataSync.SimcFileType.ScaleDataInc);
+
+            Regex regexCR = new Regex(@"__combat_ratings_mult_by_ilvl.+?\{.+?(\{.+?\}),.+?(\{.+?\}),.+?(\{.+?\}),.+?(\{.+?\}).+?\};", RegexOptions.Singleline);
+
+            Match matches = regexCR.Match(rawData);
+
+            GroupCollection groups = matches.Groups;
+
+            float[][] values = new float[4][];
+            values[0] = ParseRatingGroup(groups[1]);
+            values[1] = ParseRatingGroup(groups[1]);
+            values[2] = ParseRatingGroup(groups[1]);
+            values[3] = ParseRatingGroup(groups[1]);
+
+            var generatedData = JsonConvert.SerializeObject(values);
+
+            File.WriteAllText(
+                Path.Combine(_cacheService.BaseFileDirectory, "CombatRatingMultipliers.json"), 
+                generatedData);
+        }
+
+        void IRawDataExtractionService.GenerateStaminaMultipliers()
+        {
+            var rawData = _cacheService.GetFileContents(Model.DataSync.SimcFileType.ScaleDataInc);
+
+            Regex regexCR = new Regex(@"__stamina_mult_by_ilvl.+?\{.+?(\{.+?\}),.+?(\{.+?\}),.+?(\{.+?\}),.+?(\{.+?\}).+?\};", RegexOptions.Singleline);
+
+            Match matches = regexCR.Match(rawData);
+
+            GroupCollection groups = matches.Groups;
+
+            float[][] values = new float[4][];
+            values[0] = ParseRatingGroup(groups[1]);
+            values[1] = ParseRatingGroup(groups[1]);
+            values[2] = ParseRatingGroup(groups[1]);
+            values[3] = ParseRatingGroup(groups[1]);
+
+            var generatedData = JsonConvert.SerializeObject(values);
+
+            File.WriteAllText(
+                Path.Combine(_cacheService.BaseFileDirectory, "StaminaMultipliers.json"),
+                generatedData);
+        }
+
+        private float[] ParseRatingGroup(Group g)
+        {
+            Regex items = new Regex(@"\s+([01]\.?\d*),");
+
+            float[] values = new float[1300];
+
+            int i = 0;
+            foreach (Match m in items.Matches(g.Value))
+            {
+                values[i++] = float.Parse(m.Groups[1].Value);
+            }
+
+            return values;
+        }
+    }
+}
