4689b0f9d97fee0adb730acd35679d7ae1e1644a
Michael Wenham <niphyr@gmail.com>
2020-10-11 03:59:15 +1100

    Added DI extension method for services configuration

---
+++ SimcProfileParser/DependencyInjectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using SimcProfileParser.DataSync;
+using SimcProfileParser.Interfaces;
+using SimcProfileParser.Interfaces.DataSync;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimcProfileParser
+{
+    public static class DependencyInjectionExtensions
+    {
+        /// <summary>
+        /// Register the dependencies for the SimcProfileParser library
+        /// </summary>
+        /// <returns></returns>
+        public static IServiceCollection AddSimcProfileParser(this IServiceCollection services)
+        {
+            services.TryAddTransient<ISimcProfileParserService, SimcProfileParserService>();
+
+            // The cache is a singleton as it keeps a bunch of stuff in memory.
+            services.TryAddSingleton<ICacheService, CacheService>();
+
+            services.TryAddSingleton<IRawDataExtractionService, RawDataExtractionService>();
+            services.TryAddSingleton<ISimcParserService, SimcParserService>();
+
+            return services;
+        }
+    }
+}
