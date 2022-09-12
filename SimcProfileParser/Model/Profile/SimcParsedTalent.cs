123d5484a7434e56dd770dd0d729c6ef64d97e92
Niphyr <Niphyr@users.noreply.github.com>
2022-09-12 13:43:49 +1000

    Added basic talent parsing for dragonflight class talents

---
+++ SimcProfileParser/Model/Profile/SimcParsedTalent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace SimcProfileParser.Model.Profile
+{
+    public class SimcParsedTalent
+    {
+        public int TalentId { get; internal set; }
+        public int Rank { get; internal set; }
+
+        public SimcParsedTalent()
+        {
+
+        }
+    }
+}
